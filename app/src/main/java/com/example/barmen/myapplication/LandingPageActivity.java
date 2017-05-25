85721216c207f35dfa6e3b50000dae2b56f24ee3
Oron <oronevron@gmail.com>
2017-05-26 02:14:10 +0300

    Improve the landing page activity with the WeBrella banner - Change it to animation

--- app/src/main/java/com/example/barmen/myapplication/LandingPageActivity.java
+++ app/src/main/java/com/example/barmen/myapplication/LandingPageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class LandingPageActivity extends Activity {
 
@@ -14,6 +15,48 @@ public class LandingPageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        // Define the changing pictures
+        final int[] imageArray = { R.drawable.webrella_banner_grey_long_1, R.drawable.webrella_banner_grey_long_2, R.drawable.webrella_banner_grey_long_3};
+
+        // Get the image view of the activity
+        final ImageView activity_landing_page_imageview = (ImageView) findViewById(R.id.activity_landing_page_imageview);
+
+        // Define a handler for changing pictures
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            int i = 0;
+            int iteration_number = 0;
+
+            public void run() {
+                int interval;
+
+                activity_landing_page_imageview.setImageResource(imageArray[i]);
+                i++;
+                iteration_number++;
+                if (i > imageArray.length - 1) {
+                    i = 0;
+                }
+
+                // Make the pictures changing faster during the time
+                if (iteration_number <= 2) {
+                    interval = 350;
+                }
+                else if (iteration_number <= 5) {
+                    interval = 225;
+                }
+                else {
+                    interval = 100;
+                }
+
+                // Set timeout for interval between images
+                handler.removeCallbacks(this);
+                handler.postDelayed(this, interval);
+            }
+        };
+
+        // Set timeout for initial delay
+        handler.postDelayed(runnable, 0);
 
         // Launch the maps activity after the timeout
         new Handler().postDelayed(new Runnable() {
